ef673cc5cd5a03c00ac903d9dff425cc0f9ebd47
Magali Rousseau <rousseaumagali@gmail.com>
2020-12-30 12:12:02 +0100

    seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,8 @@ puts "> Creating users..."
 
 magali = User.new(first_name: 'Magali', last_name: 'Rousseau', email: "magali@gmail.com", password: "123456")
 
-file = URI.open("app/assets/images/magali.jpg")
-magali.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
+# file = URI.open("app/assets/images/magali.jpg")
+# magali.avatar.attach(io: file, filename: "avatar", content_type: "image/jpg")
 magali.save!
 
 puts "#{magali.first_name} created!"
